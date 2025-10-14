70f7e96fc3b8e14fd4e0c9f281ee28899b1c70af
Thura69 <spyking64@gmail.com>
2025-10-14 23:18:06 +0630

    fix(cursor)

--- app/components/common/CCursor/index.tsx
+++ app/components/common/CCursor/index.tsx
@@ -3,19 +3,15 @@ import React, { useEffect, useState } from 'react';
 import { motion, useMotionValue, useSpring } from 'framer-motion';
 
 const CCursor = () => {
-  // Raw cursor position
   const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
 
-  // Framer Motion values
   const x = useMotionValue(0);
   const y = useMotionValue(0);
 
-  // Smooth spring animation
   const springConfig = { damping: 20, stiffness: 150 };
   const smoothX = useSpring(x, springConfig);
   const smoothY = useSpring(y, springConfig);
 
-  // Track mouse
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
       setMousePos({ x: e.clientX, y: e.clientY });
@@ -27,15 +23,13 @@ const CCursor = () => {
   }, [x, y]);
 
   return (
-    <>
-      <motion.div
-        style={{
-          x: smoothX,
-          y: smoothY,
-        }}
-        className="pointer-events-none hidden lg:fixed top-0 left-0 w-6 h-6 bg-[#6817E5] rounded-full shadow-lg z-50 transform -translate-x-1/2 -translate-y-1/2"
-      />
-    </>
+    <motion.div
+      style={{
+        x: smoothX,
+        y: smoothY,
+      }}
+      className="pointer-events-none hidden lg:block fixed top-0 left-0 w-6 h-6 bg-[#6817E5] rounded-full shadow-lg z-50 transform -translate-x-1/2 -translate-y-1/2"
+    />
   );
 };
 
