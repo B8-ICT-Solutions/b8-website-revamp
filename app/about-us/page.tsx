5c8afd054f8bb655db2e0d2fa3f6f51db1c9d644
Thura69 <spyking64@gmail.com>
2025-10-17 13:10:39 +0630

    feat(seo)

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -22,7 +22,7 @@ const AboutUs = () => {
         />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://b8-website-revamp.vercel.app/about-us" />
-        <meta property="og:image" content="https://b8-website-revamp.vercel.app/og-image.jpg" />
+        <meta property="og:image" content="https://b8-website-revamp.vercel.app/logo.png" />
 
         {/* Twitter Card */}
         <meta name="twitter:card" content="summary_large_image" />
@@ -31,7 +31,7 @@ const AboutUs = () => {
           name="twitter:description"
           content="We are a team of passionate engineers dedicated to solving complex problems with innovative technology."
         />
-        <meta name="twitter:image" content="https://b8-website-revamp.vercel.app/og-image.jpg" />
+        <meta name="twitter:image" content="https://b8-website-revamp.vercel.app/logo.png" />
         <meta name="twitter:site" content="@YourTwitterHandle" />
       </Head>
 
